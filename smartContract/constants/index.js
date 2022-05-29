45b36ef96d3a5db42f3406859cf14bd4b49c320f
uzairkath <uzairkath@gmail.com>
2022-05-29 19:08:10 +0500

    metadata added for opensea

--- smartContract/constants/index.js
+++ smartContract/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x57416Ca281db2957F2612eC5C64279Cf0B57f128";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/api";
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
